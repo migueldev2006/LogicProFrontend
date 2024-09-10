514cd92026a0fd8f7aa03623ad86460dca7d0817
sofcoding2006 <nanesa1987@gmail.com>
2024-09-09 21:11:57 -0500

    añado nuevos modulos y establezco datatables y ademas de eso he añadido una alerta

--- Frontend/src/assets/js/usuario.js
+++ Frontend/src/assets/js/usuario.js
@@ -1,3 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const dataTable = new simpleDatatables.DataTable("#usuariosTable");
-});
+function showAlert() {
+    let nombre = document.getElementById("nombre").value;
+    let documento = document.getElementById("identificacion").value;
+    let correo = document.getElementById("correo").value;
+    let telefono = document.getElementById("celular").value;
+    let rol = document.getElementById("slcRol").value;
+    let cargo = document.getElementById("slcCargo").value;
+  
+    let mensaje = "Datos ingresados:\n";
+    mensaje += "Nombre Completo: " + nombre + "\n";
+    mensaje += "Documento: " + documento + "\n";
+    mensaje += "Correo Electronico: " + correo + "\n";
+    mensaje += "Telefono: " + telefono + "\n";
+    mensaje += "Rol: " + rol + "\n";
+    mensaje += "Cargo: " + cargo + "\n";
+  
+    swal("¡Registro exitoso!", mensaje, "success");
+  }
