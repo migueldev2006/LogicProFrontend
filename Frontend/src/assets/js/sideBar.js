522d28fc8b3d72495f549d82043ed5c75dc09cfe
sofcoding2006 <nanesa1987@gmail.com>
2024-09-12 23:39:18 -0500

    configuracion modulo administracion e index principal ademas el desarrollo de las alertas

--- Frontend/src/assets/js/sideBar.js
+++ Frontend/src/assets/js/sideBar.js
@@ -20,35 +20,31 @@ function loadModule(modulePath) {
       // Inicializa DataTables si estamos cargando el módulo de usuarios
       if (modulePath.includes('usuarios.html')) {
         // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
+        setTimeout(initializeDataTable, 100);
       }
       if (modulePath.includes('centros.html')) {
-        // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
+        setTimeout(initializeDataTable, 100); 
+      }
+      if (modulePath.includes('sedes.html')) {
+        setTimeout(initializeDataTable, 100);  
       }
       if (modulePath.includes('programasFormacion.html')) {
-        // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
+        setTimeout(initializeDataTable, 100);  
+      }
+      if (modulePath.includes('fichas.html')) {
+        setTimeout(initializeDataTable, 100);  
       }
       if (modulePath.includes('areas.html')) {
-        // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
+        setTimeout(initializeDataTable, 100);  
       }
       if (modulePath.includes('sitios.html')) {
-        // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
+        setTimeout(initializeDataTable, 100); 
       }
       if (modulePath.includes('tipoDeSitios.html')) {
-        // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
-      }
-      if (modulePath.includes('bodega.html')) {
-        // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
+        setTimeout(initializeDataTable, 100);  
       }
       if (modulePath.includes('municipio.html')) {
-        // Inicializa después de que el contenido ha sido agregado al DOM
-        setTimeout(initializeDataTable, 100);  // Espera para asegurarse de que el DOM esté listo
+        setTimeout(initializeDataTable, 100);  
       }
     })
     .catch((err) => console.warn("Algo salió mal al cargar el módulo.", err));
